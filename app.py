14673a7f8c5b0909ed0a70c1d60e9de3a383f217
Gary <ahumadagary.95@gmail.com>
2026-04-05 10:53:03 -0500

    primeros cambios

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ app = Flask(__name__)
 #  Se reinicia cada vez que reinicias Flask
 # ──────────────────────────────────────────
 productos = [
-    {"id": 1, "nombre": "Leche",   "cantidad": 50, "precio": 1.20},
-    {"id": 2, "nombre": "Pan",     "cantidad": 30, "precio": 0.80},
-    {"id": 3, "nombre": "Arroz",   "cantidad": 20, "precio": 0.90},
+    {"id": 1, "nombre": "cuadernoRayado",   "cantidad": 50, "precio": .50, "marca": "Norma"},
+    {"id": 2, "nombre": "cuadernoCuadriculado",     "cantidad": 30, "precio": 0.40, "marca": "Marfil"},
+    {"id": 3, "nombre": "cuadernoDobleLinea",   "cantidad": 20, "precio": 0.60, "marca": "Marfil"},
+    {"id": 4, "nombre": "lapizNegro",   "cantidad": 10, "precio": 0.10, "marca": "Mongol"},
+    {"id": 5, "nombre": "lapizRojo",   "cantidad": 15, "precio": 0.15, "marca": "Mongol"},
+    {"id": 6, "nombre": "borrador",   "cantidad": 5, "precio": 0.20, "marca": "Nata"},
+    {"id": 7, "nombre": "sacapuntas",   "cantidad": 8, "precio": 0.25, "marca": "Marfil"},
+    {"id": 8, "nombre": "regla",   "cantidad": 12, "precio": 0.30, "marca": "Imagenes"},
+    {"id": 9, "nombre": "compas",   "cantidad": 6, "precio": 0.50, "marca": "Faber-Castell"},
+    {"id": 10, "nombre": "cajaColores",   "cantidad": 4, "precio": 0.80, "marca": "Primavera"},
+    {"id": 11, "nombre": "marcadores",   "cantidad": 3, "precio": 0.90, "marca": "Primavera"},
+    {"id": 12, "nombre": "carpeta",   "cantidad": 7, "precio": 0.50, "marca": "Norma"},
+    {"id": 13, "nombre": "bolso",   "cantidad": 2, "precio": 1.50, "marca": "Totto"},
+    {"id": 14, "nombre": "cartuchera",   "cantidad": 1, "precio": 0.20, "marca": "Scribe"},
+    {"id": 15, "nombre": "calculadora",   "cantidad": 9, "precio": 1.00, "marca": "Casio"},
+    {"id": 16, "nombre": "agenda",   "cantidad": 11, "precio": 0.75, "marca": "Imagenes"},   
 ]
 siguiente_id = 4
 
@@ -29,6 +42,7 @@ def agregar():
         "nombre":   request.form["nombre"],
         "cantidad": int(request.form["cantidad"]),
         "precio":   float(request.form["precio"]),
+        "marca":    request.form["marca"]   
     })
     siguiente_id += 1
     return redirect(url_for("index"))
